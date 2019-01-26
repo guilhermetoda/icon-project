648a2c6101e22d93ec4c3d1249f3a95fbe51044b
Zack Hutchinson <zack_hutchinson747@hotmail.com>
2019-01-26 14:46:00 -0800

    scripted ceiling fan
    see above

---
+++ Assets/Script/FanRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanRotation : MonoBehaviour
+{
+    [SerializeField] public float spinSpeed = 50f;
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        transform.Rotate(0, spinSpeed * Time.deltaTime, 0);
+    }
+}
