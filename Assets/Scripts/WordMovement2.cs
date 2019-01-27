301301fd506bbcd5cbf7a76f1d0c89fd0708bdd4
staravia <destaravia@gmail.com>
2019-01-26 23:11:20 -0800

    update word movement 2

--- Assets/Scripts/WordMovement2.cs
+++ Assets/Scripts/WordMovement2.cs
@@ -1,16 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class WordMovement2 : MonoBehaviour {
+public class WordMovement2 : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private TextMeshProUGUI _textUnder;
+    private TextMeshProUGUI _textOver;
+    private Camera cam;
+
+    private void Awake()
+    {
+        //get the text component
+        _textUnder = transform.Find("Under").GetComponent<TextMeshProUGUI>();
+        _textOver = transform.Find("Over").GetComponent<TextMeshProUGUI>();
+        GameObject tempObject = GameObject.Find("MainCamera");
+        cam = tempObject.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Implements the physics of movement
+
+        bool isFullyVisible = _textUnder.GetComponent<RectTransform>().IsFullyVisibleFrom(cam);
+        if (isFullyVisible)
+        {
+            // This is a UI element since it has a RectTransform component on it
+            _textUnder.rectTransform.position += new Vector3(PlayerProgression.currentSpeed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            Debug.Log("IS NOT FULLY VIS");
+
+            CurrentWord.DestroyWord();
+            PlayerProgression.PlayerMiss();
+        }
+
+    }
+
+    public void spawnWord(string word)
+    {
+        _textUnder.text = word;
+        _textOver.text = "";
+    }
 }
