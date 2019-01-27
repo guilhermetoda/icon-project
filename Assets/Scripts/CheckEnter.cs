28d45e70e73b0bf96a4db9c389c6343a3d71951f
Leonardo de Albuquerque Hayasida <gd53leonardo@vfs.local>
2019-01-27 12:39:33 -0800

    Title-Screen
    done

---
+++ Assets/Scripts/CheckEnter.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class CheckEnter : MonoBehaviour {
+
+    //public static Typing TextBox;
+    private TextMeshProUGUI _text;
+
+    //[SerializeField] private AudioClip audioLevelUp;
+    [SerializeField] private AudioClip audioCorrectWord;
+    [SerializeField] private float delayBeforeLoading = 0.5f;
+    private AudioSource audio;
+
+    private float timeElapsed;
+    private bool activate = false;
+    private string _input = "";
+
+    private void Awake()
+    {
+        //get the text component
+        _text = GetComponent<TextMeshProUGUI>();
+        audio = GetComponent<AudioSource>();
+        //TextBox = this;
+    }
+
+    private void Update()
+    {
+        foreach (char c in Input.inputString)
+        {
+            if (c == '\b') // has backspace/delete been pressed?
+            {
+                BackSpace();
+            }
+            else if ((c == '\n') || (c == '\r')) // enter/return
+            {
+                ClearInputText();
+            }
+            // TO DO: We can't allow spacebar
+            else
+            {
+                char lowerChar = char.ToLower(c);
+                AddInputCharacter(lowerChar);
+                int indexCurrentWord = CheckCurrentWord(_text.text);
+                if (indexCurrentWord >= 0)
+                {
+                    ClearInputText();
+                    // The player just got the right word
+                    audio.PlayOneShot(audioCorrectWord);
+                    //Fadethetext.ActivateFade();
+                    Fading.BeginFade(1);
+                    activate = true;
+                }
+            }
+        }
+
+        if (activate)
+        {
+            timeElapsed += Time.deltaTime;
+        }
+        if (timeElapsed > delayBeforeLoading)
+        {
+            SceneManager.LoadScene("Home");
+        }
+    }
+    private void BackSpace()
+    {
+        if (_text.text.Length != 0)
+        {
+            _text.text = _text.text.Substring(0, _text.text.Length - 1);
+        }
+    }
+
+    private void AddInputCharacter(char input)
+    {
+        _input += input;
+        _text.text = _input;
+    }
+
+    public void ClearInputText()
+    {
+        _input = "";
+        _text.text = _input;
+    }
+
+    private int CheckCurrentWord(string word)
+    {
+        string enter = "enter";
+        if (word == enter)
+        {
+            return 1;
+        }
+        return -1;
+    }
+}
---
+++ Assets/Scripts/Fadethetext.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Fadethetext : MonoBehaviour
+{
+    [SerializeField] private Animator _anim;
+    [SerializeField] private float delayBeforeLoading = 1.5f;
+    private float timeElapsed;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("return"))
+        {
+            _anim.SetBool("activateFade", true);
+        }
+        if (_anim.GetBool("activateFade"))
+        {
+            timeElapsed += Time.deltaTime;
+        }
+        if (timeElapsed > delayBeforeLoading)
+        {
+            SceneManager.LoadScene("Home");
+        }
+    }
+}
